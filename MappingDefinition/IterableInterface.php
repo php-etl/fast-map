4c608ea7dd6a0abe27975797a48bdae1fd3a8ea0
Grégory Planchat <gregory@kiboko.fr>
2019-07-28 18:51:29 +0200

    Added Mapping classes

---
+++ MappingDefinition/IterableInterface.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Kiboko\Component\ETL\FastMap\MappingDefinition;
+
+interface IterableInterface extends MappingInterface
+{
+}
---
+++ MappingDefinition/MappingInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Kiboko\Component\ETL\FastMap\MappingDefinition;
+
+interface MappingInterface
+{
+
+}
