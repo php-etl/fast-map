112ae950fbec85542d9c377e2b8053945096a38b
Jonathan <jonathan@kiboko.fr>
2023-12-04 16:10:57 +0100

    update composer on good tag for satellite-toolbox

--- src/Compiler/Builder/PropertyPathBuilder.php
+++ src/Compiler/Builder/PropertyPathBuilder.php
@@ -6,9 +6,9 @@ namespace Kiboko\Component\FastMap\Compiler\Builder;
 
 use Kiboko\Component\SatelliteToolbox\Builder\PropertyPathBuilder;
 
-trigger_deprecation('php-etl/fast-map', '0.5', 'The "%s" class is deprecated, use "%s" instead.', 'Kiboko\\Component\\FastMap\\Compiler\\Builder\\PropertyPathBuilder', PropertyPathBuilder::class);
+trigger_deprecation('php-etl/fast-map', '0.5.2', 'The "%s" class is deprecated, use "%s" instead.', 'Kiboko\\Component\\FastMap\\Compiler\\Builder\\PropertyPathBuilder', PropertyPathBuilder::class);
 
 /*
- * @deprecated since Satellite 0.7, use Kiboko\Component\SatelliteToolbox\Builder\PropertyPathBuilder instead.
+ * @deprecated since FastMap 0.5.2, use Kiboko\Component\SatelliteToolbox\Builder\PropertyPathBuilder instead.
  */
 class_alias(PropertyPathBuilder::class, 'Kiboko\\Component\\FastMap\\Compiler\\Builder\\PropertyPathBuilder');
